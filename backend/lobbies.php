f359a73b08fdb4174d1ea46722048c8f56eaf926
torwanat <nawrotm37@gmail.com>
2024-03-10 21:57:35 +0100

    Fix finish collisions, add color translations

--- backend/lobbies.php
+++ backend/lobbies.php
@@ -6,19 +6,19 @@ require "headers.php";
 
 function sendStatus(string $status, string $nick = "", string $color = "", object $game = null)
 {
-    echo json_encode(
-        array(
-            "status" => $status,
-            "nick" => $nick,
-            "color" => $color,
-            "game" => $game
-        )
-    );
-    exit;
+	echo json_encode(
+		array(
+			"status" => $status,
+			"nick" => $nick,
+			"color" => $color,
+			"game" => $game
+		)
+	);
+	exit;
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
-    sendStatus("OPTIONS_OK");
+	sendStatus("OPTIONS_OK");
 }
 
 # data from post body
@@ -34,117 +34,117 @@ $DB_connection = new Config();
 $games = $DB_connection->getGames(); # games from database
 
 if ($post_data['nick'] == "") { # no data from session
-    sendStatus("NO_NICK"); # exits
+	sendStatus("NO_NICK"); # exits
 }
 
 if ($post_data['session']) { # request with data from session
-    foreach ($games as $row) { # find game with uid from session
-        $data = json_decode($row['data']);
-        $game = new Game($data);
-        if ($game->uid == $post_data['uid']) { ## if game found and ongoing, return it
-            if ($game->status != 2) {
-                sendStatus("OK1", $post_data['nick'], $post_data['color'], $game); # exits
-            } else {
-                break;
-            }
-        }
-    }
+	foreach ($games as $row) { # find game with uid from session
+		$data = json_decode($row['data']);
+		$game = new Game($data);
+		if ($game->uid == $post_data['uid']) { ## if game found and ongoing, return it
+			if ($game->status != 2) {
+				sendStatus("OK1", $post_data['nick'], $post_data['color'], $game); # exits
+			} else {
+				break;
+			}
+		}
+	}
 
-    # if no game found, or game ended
-    sendStatus("NO_NICK"); #exits
+	# if no game found, or game ended
+	sendStatus("NO_NICK"); #exits
 }
 
 if (empty($post_data['uid'])) {
-    foreach ($games as $row) { # check if a not full game exists
-        $data = json_decode($row['data']);
-        $game = new Game($data);
-        if (count($game->players) < 4 && $game->status == 0) { # game open to joining exists, add player
-            $nick = $post_data['nick'];
-            $color = $colors[count($game->players)];
+	foreach ($games as $row) { # check if a not full game exists
+		$data = json_decode($row['data']);
+		$game = new Game($data);
+		if (count($game->players) < 4 && $game->status == 0) { # game open to joining exists, add player
+			$nick = $post_data['nick'];
+			$color = $colors[count($game->players)];
 
-            $game->addPlayer($nick, $color);
-            if (count($game->players) == 4) {
-                $game->startGame();
-            }
-            $DB_connection->updateGame($game->uid, $game);
-            sendStatus("OK2", $nick, $color, $game); # exits
-        }
-    }
+			$game->addPlayer($nick, $color);
+			if (count($game->players) == 4) {
+				$game->startGame();
+			}
+			$DB_connection->updateGame($game->uid, $game);
+			sendStatus("OK2", $nick, $color, $game); # exits
+		}
+	}
 
-    # no open game available, create a new one
-    $game = new Game();
-    $game->uid = uniqid();
-    $color = $colors[0];
-    $nick = $post_data['nick'];
-    $game->addPlayer($nick, $color);
+	# no open game available, create a new one
+	$game = new Game();
+	$game->uid = uniqid();
+	$color = $colors[0];
+	$nick = $post_data['nick'];
+	$game->addPlayer($nick, $color);
 
-    $DB_connection->addNewGame($game);
-    sendStatus("OK3", $nick, $color, $game); # exits
+	$DB_connection->addNewGame($game);
+	sendStatus("OK3", $nick, $color, $game); # exits
 }
 
 $game;
 
 foreach ($games as $row) { # find game with uid from request
-    $data = json_decode($row['data']);
-    $game = new Game($data);
-    if ($game->uid == $post_data['uid']) {
-        break;
-    }
+	$data = json_decode($row['data']);
+	$game = new Game($data);
+	if ($game->uid == $post_data['uid']) {
+		break;
+	}
 }
 
 if (isset($post_data['type'])) { # ongoing game updated
-    $type = $post_data['type'];
-    if ($type == "STATUS") { # lobby changed (ready / unready)
-        $ready_counter = 0;
+	$type = $post_data['type'];
+	if ($type == "STATUS") { # lobby changed (ready / unready)
+		$ready_counter = 0;
 
-        foreach ($game->players as $player) {
-            if ($player->color == $post_data['color']) {
-                if ($player->status == 0) {
-                    $player->status = 1;
-                } else {
-                    $player->status = 0;
-                }
-            }
-            if ($player->status == 1) {
-                $ready_counter++;
-            }
-        }
+		foreach ($game->players as $player) {
+			if ($player->color == $post_data['color']) {
+				if ($player->status == 0) {
+					$player->status = 1;
+				} else {
+					$player->status = 0;
+				}
+			}
+			if ($player->status == 1) {
+				$ready_counter++;
+			}
+		}
 
-        # if everyone ready, start game
-        if ($ready_counter >= 2 || count($game->players) == 4) {
-            $game->startGame();
-        }
-    } else {
-        if (isset($post_data['pawns'])) {
-            $game->pawns = $post_data['pawns'];
-        }
-        if (isset($post_data['roll'])) {
-            $game->roll = $post_data['roll'];
-        }
-        switch ($type) {
-            case "MOVE":
-                $game->nextPlayerTurn();
-                break;
-            case "ROLL":
-                $game->diceThrowMade($post_data['color']);
-                break;
-            case "WIN":
-                $game->gameWon($post_data['color']);
-                break;
-            default:
-                break;
-        }
-    }
+		# if everyone ready, start game
+		if ($ready_counter >= 2 || count($game->players) == 4) {
+			$game->startGame();
+		}
+	} else {
+		if (isset($post_data['pawns'])) {
+			$game->pawns = $post_data['pawns'];
+		}
+		if (isset($post_data['roll'])) {
+			$game->roll = $post_data['roll'];
+		}
+		switch ($type) {
+			case "MOVE":
+				$game->nextPlayerTurn();
+				break;
+			case "ROLL":
+				$game->diceThrowMade($post_data['color']);
+				break;
+			case "WIN":
+				$game->gameWon($post_data['color']);
+				break;
+			default:
+				break;
+		}
+	}
 
-    $DB_connection->updateGame($post_data['uid'], $game);
-    sendStatus("OK4"); # exits
+	$DB_connection->updateGame($post_data['uid'], $game);
+	sendStatus("OK4"); # exits
 } else {
-    if (isset($game) && $game->timestamp > 0 && time() - $game->timestamp >= 60) {
-        $game->nextPlayerTurn();
-        $DB_connection->updateGame($post_data['uid'], $game);
-        sendStatus("OK6", $post_data['nick'], $post_data['color'], $game); # exits
-    }
-    sendStatus("OK7", $post_data['nick'], $post_data['color'], $game); # exits
+	if (isset($game) && $game->timestamp > 0 && time() - $game->timestamp >= 60 && $game->status != 2) {
+		$game->nextPlayerTurn();
+		$DB_connection->updateGame($post_data['uid'], $game);
+		sendStatus("OK6", $post_data['nick'], $post_data['color'], $game); # exits
+	}
+	sendStatus("OK7", $post_data['nick'], $post_data['color'], $game); # exits
 
-    sendStatus("NO_NICK"); # error happened
+	sendStatus("NO_NICK"); # error happened
 }
--- backend/move.php
+++ backend/move.php
@@ -3,8 +3,8 @@ require "game_data.php";
 require "headers.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
-    http_response_code(200);
-    exit;
+	http_response_code(200);
+	exit;
 }
 
 $rawInput = fopen('php://input', 'r');
@@ -23,70 +23,70 @@ $win = false;
 
 function sendResult(string $result, array $pawns)
 {
-    echo json_encode(
-        array(
-            "status" => $result,
-            "pawns" => $pawns
-        )
-    );
-    exit;
+	echo json_encode(
+		array(
+			"status" => $result,
+			"pawns" => $pawns
+		)
+	);
+	exit;
 }
 
 if ($pawn_color != $player_color) {
-    sendResult("NO", $pawns);
+	sendResult("NO", $pawns);
 }
 
 # move pawn
 if (in_array($tile_id, $game_path)) {
-    $moved = array_search($tile_id, $game_path) - $offsets[$pawn_color];
-    if ($moved < 0) {
-        $moved += count($game_path);
-    }
+	$moved = array_search($tile_id, $game_path) - $offsets[$pawn_color];
+	if ($moved < 0) {
+		$moved += count($game_path);
+	}
 
-    foreach ($pawns as $index => $pawn) {
-        if ($pawn['moved'] == $moved && $pawn['color'] == $pawn_color) {
-            if ($moved + $roll_result >= count($game_path) + count($finishes[$pawn_color])) {
-                sendResult("NO", $pawns);
-            } else {
-                $pawns[$index]['moved'] += $roll_result;
-            }
-            break;
-        }
-    }
+	foreach ($pawns as $index => $pawn) {
+		if ($pawn['moved'] == $moved && $pawn['color'] == $pawn_color) {
+			if ($moved + $roll_result >= count($game_path) + count($finishes[$pawn_color])) {
+				sendResult("NO", $pawns);
+			} else {
+				$pawns[$index]['moved'] += $roll_result;
+			}
+			break;
+		}
+	}
 } else {
-    $spawn = false;
-    foreach ($spawns as $color => $spawn_tiles) {
-        if (in_array($tile_id, $spawn_tiles)) {
-            $spawn = true;
-            break;
-        }
-    }
+	$spawn = false;
+	foreach ($spawns as $color => $spawn_tiles) {
+		if (in_array($tile_id, $spawn_tiles)) {
+			$spawn = true;
+			break;
+		}
+	}
 
-    if ($spawn) {
-        if ($roll_result != 6 && $roll_result != 1) {
-            sendResult("NO", $pawns);
-        }
+	if ($spawn) {
+		if ($roll_result != 6 && $roll_result != 1) {
+			sendResult("NO", $pawns);
+		}
 
-        $spawn_index = array_search($tile_id, $spawns[$pawn_color]);
-        foreach ($pawns as $index => $pawn) {
-            if ($pawn['moved'] == -1 && $pawn['color'] == $pawn_color && $index % 4 == $spawn_index) {
-                $pawns[$index]['moved'] = 0;
-                break;
-            }
-        }
-    } else {
-        $finish_index = array_search($tile_id, $finishes[$pawn_color]);
-        foreach ($pawns as $index => $pawn) {
-            if ($pawn['moved'] - count($game_path) == $finish_index && $pawn['color'] == $pawn_color) {
-                if ($finish_index + $roll_result >= count($finishes[$pawn_color])) {
-                    sendResult("NO", $pawns);
-                } else {
-                    $pawns[$index]['moved'] += $roll_result;
-                }
-                break;
-            }
-        }
-    }
+		$spawn_index = array_search($tile_id, $spawns[$pawn_color]);
+		foreach ($pawns as $index => $pawn) {
+			if ($pawn['moved'] == -1 && $pawn['color'] == $pawn_color && $index % 4 == $spawn_index) {
+				$pawns[$index]['moved'] = 0;
+				break;
+			}
+		}
+	} else {
+		$finish_index = array_search($tile_id, $finishes[$pawn_color]);
+		foreach ($pawns as $index => $pawn) {
+			if ($pawn['moved'] - count($game_path) == $finish_index && $pawn['color'] == $pawn_color) {
+				if ($finish_index + $roll_result >= count($finishes[$pawn_color])) {
+					sendResult("NO", $pawns);
+				} else {
+					$pawns[$index]['moved'] += $roll_result;
+				}
+				break;
+			}
+		}
+	}
 }
 
 # check for collision
@@ -94,66 +94,67 @@ $tiles = array();
 $collision_tile = -1;
 
 foreach ($pawns as $index => $pawn) {
-    $temp_tile = 0;
-    if ($pawn['moved'] >= count($game_path)) {
-        $temp_tile = $finishes[$pawn['color']][$pawn['moved'] - count($game_path)];
-    } else if ($pawn['moved'] < 0) {
-        $temp_tile = $spawns[$pawn['color']][$index % 4];
-    } else {
-        $temp_tile = $game_path[($pawn['moved'] + $offsets[$pawn['color']]) % count($game_path)];
-    }
+	$temp_tile = 0;
+	if ($pawn['moved'] >= count($game_path)) {
+		$temp_tile = $finishes[$pawn['color']][$pawn['moved'] - count($game_path)];
+	} else if ($pawn['moved'] < 0) {
+		$temp_tile = $spawns[$pawn['color']][$index % 4];
+	} else {
+		$temp_tile = $game_path[($pawn['moved'] + $offsets[$pawn['color']]) % count($game_path)];
+	}
 
-    foreach ($tiles as $tile) {
-        if ($tile['id'] == $temp_tile && $tile['color'] != $pawn['color']) {
-            $collision_tile = $temp_tile;
-            break 2;
-        }
-    }
-    array_push(
-        $tiles,
-        array(
-            "id" => $temp_tile,
-            "color" => $pawn['color']
-        )
-    );
+	foreach ($tiles as $tile) {
+		if ($tile['id'] == $temp_tile) {
+			if ($tile['color'] != $pawn['color']) {
+				$collision_tile = $temp_tile;
+				break 2;
+			}
+			foreach ($finishes as $index => $finish_tiles) {
+				if (in_array($collision_tile, $finish_tiles)) {
+					sendResult("NO", $pawns);
+				}
+			}
+		}
+	}
+	array_push(
+		$tiles,
+		array(
+			"id" => $temp_tile,
+			"color" => $pawn['color']
+		)
+	);
 }
 
 if ($collision_tile != -1) {
-    foreach ($finishes as $index => $finish_tiles) {
-        if (in_array($collision_tile, $finish_tiles)) {
-            sendResult("NO", $pawns);
-        }
-    }
-
-    foreach ($pawns as $index => $pawn) {
-        $temp_tile = 0;
-        if ($pawn['moved'] >= count($game_path)) {
-            $temp_tile = $finishes[$pawn['color']][$pawn['moved'] - count($game_path)];
-        } else if ($pawn['moved'] < 0) {
-            $temp_tile = $spawns[$pawn['color']][$index % 4];
-        } else {
-            $temp_tile = $game_path[($pawn['moved'] + $offsets[$pawn['color']]) % count($game_path)];
-        }
-        if ($temp_tile == $collision_tile && $pawn['color'] != $pawn_color) {
-            $pawns[$index]['moved'] = -1;
-        }
-    }
+	foreach ($pawns as $index => $pawn) {
+		$temp_tile = 0;
+		if ($pawn['moved'] >= count($game_path)) {
+			$temp_tile = $finishes[$pawn['color']][$pawn['moved'] - count($game_path)];
+		} else if ($pawn['moved'] < 0) {
+			$temp_tile = $spawns[$pawn['color']][$index % 4];
+		} else {
+			$temp_tile = $game_path[($pawn['moved'] + $offsets[$pawn['color']]) % count($game_path)];
+		}
+		if ($temp_tile == $collision_tile && $pawn['color'] != $pawn_color) {
+			$pawns[$index]['moved'] = -1;
+		}
+	}
 }
 
 #check for win
 $pawns_finished = [0, 0, 0, 0];
 foreach ($pawns as $index => $pawn) {
-    if ($pawn['moved'] > count($game_path)) {
-        $pawns_finished[floor($index / 4)] += 1;
-    }
+	if ($pawn['moved'] > count($game_path)) {
+		$pawns_finished[floor($index / 4)] += 1;
+	}
 }
 
 if (in_array(4, $pawns_finished)) {
-    $win = true;
+	$win = true;
 }
 
 if ($win) {
-    sendResult("WIN", $pawns);
+	sendResult("WIN", $pawns);
 } else {
-    sendResult("MOVE", $pawns);
+	sendResult("MOVE", $pawns);
 }
