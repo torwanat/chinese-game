ef8a2c7dd2e0d6e6eb9ccc23bbd0658a2745f6e3
torwanat <nawrotm37@gmail.com>
2026-07-10 23:39:14 +0200

    update PHP headers

--- backend/headers.php
+++ backend/headers.php
@@ -1,3 +1,3 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:4200");
+header("Access-Control-Allow-Origin: https://chinese.torvan.eu");
 header("Access-Control-Allow-Headers: Content-Type");
